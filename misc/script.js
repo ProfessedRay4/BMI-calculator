4412e6bd890c0df781e22f04fbf0ce265ac226f3
ProfessedRay4 <jeremiahbanks2026@gmail.com>
2024-09-22 12:49:23 -0500

    bug fixes and some small changes

--- misc/script.js
+++ misc/script.js
@@ -1,16 +1,29 @@
+var elem = document.getElementById("weight");
+elem.onkeyup = function(e){
+    if(e.keyCode == 13){
+        calculateBMI();
+    }
+} 
+
 function calculateBMI() {
     //Formula: weight (lb) / [height (in)]2 x 703
     // check if input is valid
 
-    if (document.getElementById('weight').value <= 5 || document.getElementById('weight').value >= 1400 || document.getElementById('height').value <= 21 || document.getElementById('height').value >= 100) {
+    if (document.getElementById('weight').value <= 4 || document.getElementById('weight').value >= 1401 || document.getElementById('height').value <= 20 || document.getElementById('height').value >= 101) {
         document.getElementById("result").innerHTML = "Invalid input";
         document.getElementById("result").style.color = "red";
         return;
     } else { // if input is valid calculate!
         var weight = document.getElementById('weight').value;
         var height = document.getElementById('height').value;
-        console.log(Math.round((weight / (height * height)) * 703));
-        document.getElementById("result").innerHTML = "Your BMI is " + Math.round((weight / (height * height)) * 703) + " lbs";
-        document.getElementById("result").style.color = "#333";
+        var bmi = (weight / (height * height)) * 703;
+        console.log(Math.round(bmi));
+        document.getElementById("result").innerHTML = "Your BMI is " + Math.round(bmi) + " lbs";
+
+        if (bmi >= 18.5 && bmi <= 24.9) { // if its not 18.5 - 24.9 its unhealthy
+            document.getElementById("result").style.color = "#228B22"; // green
+        } else {
+            document.getElementById("result").style.color = "#C41E3A"; // red
+        }
     }
 }
